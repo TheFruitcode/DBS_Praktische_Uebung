eabf5acc57ae3f0d4a11a6510070ad49e303da85
fare3020 <falco.rensner@study.hs-duesseldorf.de>
2026-01-13 17:56:39 +0100

    Start

---
+++ src/backend/main.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Student(
+    student_id INT NOT NULL, 
+    name VARCHAR(150),
+    email VARCHAR(100),
+    PRIMARY KEY (student_id)
+);
+
+CREATE TABLE Dozent(
+    dozent_id INT NOT NULL,
+    name VARCHAR(150),
+    department VARCHAR(80),
+    PRIMARY KEY (dozent_id)
+);
+
+CREATE TABLE Kurs(
+    kurs_id INT NOT NULL,
+    title VARCHAR(50),
+    dozent_id INT,
+    PRIMARY KEY (kurs_id),
+    FOREIGN KEY (dozent_id) REFERENCES Dozent(dozent_id)
+);
+
+CREATE TABLE Anmeldungen(
+    student_id INT,
+    kurs_id INT,
+    semester INT,
+    FOREIGN KEY (student_id) REFERENCES Student(student_id),
+    FOREIGN KEY (kurs_id) REFERENCES Kurs(kurs_id)
+);
